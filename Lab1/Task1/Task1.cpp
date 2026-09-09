f3a18346df9ec35e5eee794f941220a5f4217f21
Niktokton <nikita.konovalov-2006@mail.ru>
2026-09-09 21:33:28 +0500

    fillArray and printArray realisation

---
+++ Lab1/Task1/Task1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void fillArray(int(&arr)[10])
+{
+    for (int& x : arr)
+    {
+        x = rand() % 100;
+    }
+}
+
+void printArray(int(&arr)[10])
+{
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[10];
+    fillArray(arr);
+    printArray(arr);
+    return 0;
+}
