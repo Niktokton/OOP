f01a4f9fc146343377aeb942741f14e9307337cb
Niktokton <nikita.konovalov-2006@mail.ru>
2026-09-17 13:57:14 +0500

    Финальная Доработка Doxygen

--- Lab1/docs/html/navtreedata.js
+++ Lab1/docs/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "Lab 1", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
